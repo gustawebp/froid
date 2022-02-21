de351720e72a4c18102dc41c246782a8689d6726
Gustavo zero bala <98385777+gustawebp@users.noreply.github.com>
2022-02-21 18:31:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ var hora1= hora.getHours()
 var min = new Date
 var min1 = min.getMinutes()
 var txt = document.getElementById('hor')
+var txt1 = document.getElementById('hor1')
 txt.innerHTML = ` ${hora1} horas e ${min1} minutos `
+
+if (hora1 > 12) {txt1.innerHTML = 'Boa Tarde'}
+if (hora1 > 1 && hora1 < 12 ) {txt1.innerHTML='Bom dia'}
+
+if ( hora1 > 18 && hora1 < 23) { txt1.innerHTML = 'Boa noite'}
+txt.innerHTML = `Agora são ${hora1}  e ${min1} minutos`
